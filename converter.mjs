ef7a4cf4b2e46d52cd9e84b3d487dec23970f1da
Mikhail Komyshev <rest.liandri@gmail.com>
2020-06-04 18:43:19 +0300

    fix mistake

--- converter.mjs
+++ converter.mjs
@@ -1,24 +1,24 @@
 ﻿export function dnaToRna(s) {
-  let resoultRNA = '';
+  let result = '';
   if (s == null) return null;
   if (s == '') return '';
   for (let c of s.toUpperCase()) {
     switch(c) {
       case 'G':
-        resoultRNA += 'C';
+        result += 'C';
         break;
       case 'C':
-        resoultRNA += 'G';
+        result += 'G';
         break;
       case 'T':
-        resoultRNA += 'A';
+        result += 'A';
         break;
       case 'A':
-        resoultRNA += 'U';
+        result += 'U';
         break;
       default:
         throw new Error('ValidationError');
     }
   }
-  return resoultRNA;
+  return result;
 }
